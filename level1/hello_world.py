1b5d29a278ea2f639eaed73247cac46869f4872c
Zoee333 <167110681+Zoee333@users.noreply.github.com>
2025-04-09 10:55:09 +0200

    task 2 test

---
+++ level1/hello_world.py
@@ -0,0 +1,12 @@
+# Pseudocode:
+# Request name from user
+# Print name
+# Request age from user
+# Print age
+# Print "Hello, World!"
+
+name = input("Please enter your name: ")
+print(name)
+age = input("Please enter your age: ")
+print(age)
+print("Hello, World!")
