768f4f19ea243d22291122b8b2a2413544c0fa34
Zoee333 <zoeherman1204@gmail.com>
2025-04-11 21:53:10 +0200

    task 3b test

---
+++ level1/task_3b.py
@@ -0,0 +1,23 @@
+# Declare given variables of values 99.23, 23, 150, "100"
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Convert num1 into an integer
+num1 = int(num1)
+
+# Convert num2 into a float
+num2 = float(num2)
+
+# Convert num3 into a string
+num3 = str(num3)
+
+# Convert string1 into an integer
+string1 = int(string1)
+
+# Print each variable on individual lines
+print(num1)
+print(num2)
+print(num3)
+print(string1)
