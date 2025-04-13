3511a743db35680660b394f1694d026b40288fbc
Zoee333 <zoeherman1204@gmail.com>
2025-04-13 20:55:43 +0200

    task4a-test

---
+++ level1/task4a.py
@@ -0,0 +1,13 @@
+# Assign a single strin: “The!quick!brown!fox!jumps!over!the!lazy!dog.”
+single_string = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+
+# Replace "!" with a space, then print
+single_string = single_string.replace("!", " ")
+print(single_string)
+
+# Rewrite with all characters as capitals, then print
+single_string = single_string.upper()
+print(single_string)
+
+# Print string in reverse
+print(single_string[::-1])
